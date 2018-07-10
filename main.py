c47cd808d729a7926f6e531871e4732fe97fe07c
Pedro <hablutzel94@gmail.com>
2018-07-10 12:47:58 -0500

    v4 all

--- main.py
+++ main.py
@@ -1,96 +1,169 @@
+from random import randint
 
-Z = ["Z1" , "Z2" , "Z3" , "Z4"] # N ZONAS
+Z = ["Z1", "Z2", "Z3", "Z4"]  # N ZONAS
+TZ = ["A" , "A" , "P" , "G"] # TIPO DE ZONA
 
-C = ["C1" , "C2" , "C3"] # M CARROR EN SOLICITUD DE ESTACIONAMIENTO EN UN TIEMPO DETERMINADO
+# M CARROR EN SOLICITUD DE ESTACIONAMIENTO EN UN TIEMPO DETERMINADO
+C = ["C1", "C2", "C3", "C4", "C5"]
+TC = ["P" , "A" , "A" , "G", "A"] # TIPO DE CARRO
 
-D = [   [0.24 , 0.48 , 0.20 , 0.18 ], # MATRIZ DE DISTANCIAS DE LOS CARROS HACIA LAS ZONAS DE ESTACIONAMIENTO
-        [0.16 , 0.13 , 0.02 , 0.26],
-        [0.17 , 0.6 , 0.14 , 0.3]
-    ]
-D2 = [   [0.24 , 0.48 , 0.20 , 0.18 ], # MATRIZ DE DISTANCIAS DE LOS CARROS HACIA LAS ZONAS DE ESTACIONAMIENTO
-        [0.16 , 0.13 , 0.02 , 0.26],
-        [0.17 , 0.6 , 0.14 , 0.3]
-    ]
+D = [[0.24, 0.48, 0.20, 0.18],  # MATRIZ DE DISTANCIAS DE LOS CARROS HACIA LAS ZONAS DE ESTACIONAMIENTO
+     [0.16, 0.13, 0.02, 0.26],
+     [0.15, 0.61, 0.01, 0.52],
+     [0.19, 0.54, 0.23, 0.45],
+     [0.23, 0.73, 0.33, 0.31]
+     ]
 
-FS1 = [3,1,0,6] # CANTIDAD DE SPOTS LIBRES POR ZONA
-FS2 = [3,1,0,6]
-FS3 = [3,1,0,6]
+T = [[10, 3, 1, 5],  # MATRIZ DE TIEMPO QUE TOMA A CADA CARRO LLEGAR A LAS ZONAS EN MINUTOS
+     [7, 8, 2, 5],
+     [2, 7, 2, 2],
+     [6, 3, 8, 3],
+     [2, 8, 1, 8]
+     ]
 
-A1 = [] # A = (aij)M×N
-A2 = [] # A = (aij)M×N
-A3 = [] # A = (aij)M×N
 
-for m in range(len(C)): # CREAR MATRIZ DE ASIGNACION EN BLANCO
+FS = [39, 18, 51, 63]  # CANTIDAD DE SPOTS LIBRES POR ZONA
+
+WR = [[0.02, 0.78, 0.02, 0.18],
+      [0.03, 0.43, 0.32, 0.83],
+      [0.05, 0.43, 0.74, 0.72],
+      [0.06, 0.13, 0.23, 0.16],
+      [0.03, 0.64, 0.32, 0.67]
+      ]
+
+apha = [0, 0.25, 0.75, 0.35, 0.15]
+beta = [1, 0.75, 0.25, 0.65, 0.85]
+
+A = []  # A = (aij)M×N
+
+for m in range(len(C)):  # CREAR MATRIZ DE ASIGNACION EN BLANCO
     fila = []
     for n in range(len(Z)):
         fila.append(0)
-    A1.append(fila)
-    A2.append(fila)
-    A3.append(fila)
+    A.append(fila)
 
-def fs_availability(zona , fs):
-    if fs[zona] > 0:
-        fs[zona]= fs[zona] - 1
+def clone(array):
+    a = []
+    for e in array:
+        a.append(e)
+    return a
+
+def get_positions(array, original):
+    v = []
+    for a in array:
+        i = original.index(a)
+        v.append(i)
+    return v
+
+def get_slice_array(array, indexs):
+    v = []
+    for i in indexs:
+        v.append(array[i])
+    return v
+
+def get_not_zonas_type(type):
+    zn = []
+    for i,z in enumerate(Z):
+        if type != TZ[i]:
+            zn.append(i)
+    return zn
+
+def is_available(zona):
+    if FS[zona] > 0:
         return True
     else:
         return False
 
-def calcular_menor_v1(carro):
-    val = 0
-    aux = D[carro].copy()
-    D[carro].sort()
-    find = False
-    while not find:
-        pos_min = aux.index(D[carro][val])
-        if fs_availability(pos_min , FS1):
-            find = True
-            print("**Carro: {}, asignado a la zona : {}".format(carro,pos_min))
-            A1[carro][pos_min] = 1 # ASIGNAR 1 A LA SELECCIONADA
-            print(FS1)  
-            return
-        print("No hay spot disponible en la zona {}. Recalculando ...".format(pos_min))
-        val+=1
-
-def llenar_matriz_asignacion_v1():
+def update_fs(fs):
+    #ASUMIR QUE EL TIEMPO QUE LE FALTA A LOS CARROS ESTACIONADOS PARA IRSE RANDOM
     for c in range(len(C)):
-        calcular_menor_v1(c)
-    print(A1)
+        for z in range(len(Z)):
+            leave = randint(0, 20)
+            t = T[c][z]
+            if leave < t:
+                fs[z] += 1
+    return fs
 
-def funcion_costo_v1():
-    suma = 0
-    for m in range(len(C)):
-        for n in range(len(Z)):
-            suma += D2[m][n] * A1[m][n]
-    print("\n F1) Valor minimo de la funcion costo : {}".format(suma))
+def take_spot(zona,fs):
+    fs[zona] -= 1
+    return fs
 
+def get_ideal_v1(zonas, skip=[]):
+    z = -1
+    indexs = sorted(range(len(zonas)), key=zonas.__getitem__)
+    for i in indexs:
+        if i not in skip:
+            if is_available(i):
+                z = i
+                break
+    return z
 
-llenar_matriz_asignacion_v1()
+def get_ideal_v2(zonas, carro, skip=[]):
+    zn = -1
+    v = []
+    for i, z in enumerate(zonas):
+        s = z*apha[carro] + beta[carro]*WR[carro][i]
+        v.append(s)
+    indexs = sorted(range(len(v)), key=v.__getitem__)
+    for i in indexs:
+        if i not in skip:
+            if v.count(v[i]) > 1:
+                f = list(filter((v[i]).__ne__, v))
+                zn = get_ideal_v1(zonas, get_positions(f, v) + skip)
+                break
+            if is_available(i):
+                zn = i
+                break
+    return zn
 
-#********************************************
+def get_ideal_v3(zonas,carro):
+    z = -1
+    tc = TC[carro]
+    skip = get_not_zonas_type(tc)
+    z = get_ideal_v2(zonas, carro, skip)
+    return z
 
-WR = [0.02 , 0.02 , 0.78 , 0.18]
-apha = [0 , 0.25 , 0.75]
-beta = [1 , 0.75 , 0.25]
+def get_ideal_v4(zonas,carro):
+    z = get_ideal_v3(zonas, carro)
+    return z
 
-def calcular_matriz_asignacion_v2(carro):
-    aux = []
-    suma = 0
-    for n in range(len(Z)):
-        suma = D2[carro][n] * apha[carro] + beta[carro] * WR[n]
-        aux.append(suma)
-    print(aux)
-    print("Menor para el carro {} : {}".format(carro , min(aux)))
+def run_example_v1():
+    fs = clone(FS)
+    print("Cantidad de espacios libres  iniciales  : {}".format(fs))
+    for i,c in enumerate(C):
+        ideal = get_ideal_v1(D[i])
+        take_spot(ideal,fs)
+        print("La  mejor zona para el carro {} es => {}".format(i,ideal))
+    print("Cantidad de espacios libres  restantes  : {}".format(fs))
 
-def consultar_matriz_v1(array , carro):
-    for p in array:
-        if A1[carro][p] == 1:
-            return p
+def run_example_v2():
+    fs = clone(FS)
+    print("Cantidad de espacios libres  iniciales  : {}".format(fs))
+    for i,c in enumerate(C):
+        ideal = get_ideal_v2(D[i],i)
+        print(ideal)
+        take_spot(ideal,fs)
+        print("La  mejor zona para el carro {} es => {}".format(i,ideal))
+    print("Cantidad de espacios libres  restantes  : {}".format(fs))
 
-def funcion_costo_v2():
-    suma = 0
-    for m in range(len(C)):
-        for n in range(len(Z)):
-            suma += D2[m][n] * A2[m][n] * apha[m] + A2[m][n] * beta[m] * WR[n]
-    print("\n F2) Valor minimo de la funcion costo : {}".format(suma))
+def run_example_v3():
+    fs = clone(FS)
+    print("Cantidad de espacios libres  iniciales  : {}".format(fs))
+    for i,c in enumerate(C):
+        ideal = get_ideal_v3(D[i],i)
+        take_spot(ideal,fs)
+        print("La  mejor zona para el carro {} es => {}".format(i,ideal))
+    print("Cantidad de espacios libres  restantes  : {}".format(fs))
 
-calcular_matriz_asignacion_v2(0)
+def run_example_v4():
+    fs = clone(FS)
+    print("Cantidad de espacios libres  iniciales  : {}".format(fs))
+    fs = update_fs(fs)
+    print("Cantidad de espacios libres  actualizados  : {}".format(fs))
+    for i,c in enumerate(C):
+        ideal = get_ideal_v4(D[i],i)
+        take_spot(ideal,fs)
+        print("La  mejor zona para el carro {} es => {}".format(i,ideal))
+    print("Cantidad de espacios libres  restantes  : {}".format(fs))
+
+run_example_v4()
